27742117b7d5b94f632975c63d39b4ccd0ec126d
unknown <hiro@.(none)>
2010-03-25 04:15:11 +0900

    Add ape.h & release file(bass_tak2.4.dll)

---
+++ src/ape.h
@@ -0,0 +1,38 @@
+
+/*****************************************************************************************
+"Standard" APE tag fields
+*****************************************************************************************/
+#define APE_TAG_FIELD_TITLE                     "Title"
+#define APE_TAG_FIELD_ARTIST                    "Artist"
+#define APE_TAG_FIELD_ALBUM                     "Album"
+#define APE_TAG_FIELD_COMMENT                   "Comment"
+#define APE_TAG_FIELD_YEAR                      "Year"
+#define APE_TAG_FIELD_TRACK                     "Track"
+#define APE_TAG_FIELD_GENRE                     "Genre"
+#define APE_TAG_FIELD_COVER_ART_FRONT           "Cover Art (front)"
+#define APE_TAG_FIELD_NOTES                     "Notes"
+#define APE_TAG_FIELD_LYRICS                    "Lyrics"
+#define APE_TAG_FIELD_COPYRIGHT                 "Copyright"
+#define APE_TAG_FIELD_BUY_URL                   "Buy URL"
+#define APE_TAG_FIELD_ARTIST_URL                "Artist URL"
+#define APE_TAG_FIELD_PUBLISHER_URL             "Publisher URL"
+#define APE_TAG_FIELD_FILE_URL                  "File URL"
+#define APE_TAG_FIELD_COPYRIGHT_URL             "Copyright URL"
+#define APE_TAG_FIELD_MJ_METADATA               "Media Jukebox Metadata"
+#define APE_TAG_FIELD_TOOL_NAME                 "Tool Name"
+#define APE_TAG_FIELD_TOOL_VERSION              "Tool Version"
+#define APE_TAG_FIELD_PEAK_LEVEL                "Peak Level"
+#define APE_TAG_FIELD_REPLAY_GAIN_RADIO         "Replay Gain (radio)"
+#define APE_TAG_FIELD_REPLAY_GAIN_ALBUM         "Replay Gain (album)"
+#define APE_TAG_FIELD_COMPOSER                  "Composer"
+#define APE_TAG_FIELD_KEYWORDS                  "Keywords"
+
+/*****************************************************************************************
+Standard APE tag field values
+*****************************************************************************************/
+#define APE_TAG_GENRE_UNDEFINED                 "Undefined"
+
+/*****************************************************************************************
+The footer at the end of APE tagged files (can also optionally be at the front of the tag)
+*****************************************************************************************/
+#define APE_TAG_FOOTER_BYTES    32
